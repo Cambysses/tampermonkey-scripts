934bcd52fb8f9c4e3d066613889f939419bd7f9a
Cambysses <selxxa@gmail.com>
2019-07-26 15:42:28 -0300

    Update script.js
    Uses querySelector instead of getElementByXpath

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
 // ==UserScript==
 // @name     Latest Tweets
-// @version  1.1
+// @version  1.2
 // @grant    none
 // @include http*://twitter.com*
 // @run-at document-end
 // ==/UserScript==
 
 // Script sees if tweets are set to "home", and if so switches to "latest".
-
-function getElementByXpath(path)
-{
-	return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-}
 
 function sleep(ms)
 {
@@ -20,30 +15,30 @@ function sleep(ms)
 
 function click(element)
 {
-  var button = getElementByXpath(element);
-  var event = document.createEvent("HTMLEvents");
-  event.initEvent("click", true, true);
-  button.dispatchEvent(event);	 
+	let event = document.createEvent("HTMLEvents");
+	event.initEvent("click", true, true);
+	element.dispatchEvent(event);	 
 }
 
 (async () => 
- {
-  for(i = 0; i < 5; i++)
-  {
-    await sleep(1000);
+{
+	for(i = 0; i < 5; i++)
+	{
+		await sleep(1000);
 
-    // Checks if tweets are set to "Home".
-    var home = (getElementByXpath("/html/body/div/div/div/div/main/div/div[2]/div/div[1]/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/h2/span").innerText);
-    if (home == "Home")
-    {
-      // Clicks on option icon.
-      click("/html/body/div/div/div/div/main/div/div[2]/div/div[1]/div/div/div[1]/div[1]/div/div/div/div/div[2]/div/div");
-      await sleep(500);
+		// Checks if tweets are set to "Home".
+		let menuButton = document.querySelector('div[aria-label="Top Tweets on"]')
+		if (menuButton != null)
+		{
+			// Clicks on option icon.
+			click(menuButton);
+			await sleep(500);
 
-      // Click "Latest".
-      click("/html/body/div/div/div/div[1]/div/div/div[2]/div[3]/div/div/div/div[2]");
-      break
-    }
-    console.log(i);
-  }
+			// Click "Latest".
+			let latestButton = document.querySelector('div[role="menuitem"]')
+			click(latestButton);
+			break
+		}
+		console.log(i);
+	}
 })();
